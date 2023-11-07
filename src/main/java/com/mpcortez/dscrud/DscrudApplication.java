3f77aee11b4121b3ca8dad8acd21c9fd6ebee496
Mpcortez <murilo_pass@hotmail.com>
2023-11-07 08:49:14 -0300

    Set locale

--- src/main/java/com/mpcortez/dscrud/DscrudApplication.java
+++ src/main/java/com/mpcortez/dscrud/DscrudApplication.java
@@ -1,13 +1,23 @@
 package com.mpcortez.dscrud;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Locale;
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class DscrudApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DscrudApplication.class, args);
-	}
+    @PostConstruct
+    public void init() {
+        Locale.setDefault(new Locale("pt", "BR"));
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(DscrudApplication.class, args);
+    }
 
 }
